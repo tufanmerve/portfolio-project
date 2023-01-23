0c02ea618242c19fddc660227e5050949bb8903f
tufanmerve <merve2fan@gmail.com>
2023-01-23 20:37:05 +0300

    CardDeal section created

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { feedback } from '../constants';
+import styles from '../style';
+import FeedbackCard from './FeedbackCard';
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <div>
+      
+    </div>
   )
 }
 
